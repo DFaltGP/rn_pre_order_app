f97549bf771680a2fb354e65e1b29cdf68266849
DFaltGP <skyzzlockzz@gmail.com>
2024-02-09 16:02:00 -0300

    utilitário conversor de moedas

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,9 +1,23 @@
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
 import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
-  console.log(id);
-
-  return <View className="flex-1"></View>;
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      />
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+      </View>
+    </View>
+  );
 }
