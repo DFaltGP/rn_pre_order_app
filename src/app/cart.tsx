efe1817f77eeabb1feeef14aaccd934daaef0e42
DFaltGP <skyzzlockzz@gmail.com>
2024-02-10 15:49:16 -0300

    criando método de remover do carrinho e usando o Alert do RN

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,15 +1,30 @@
-import { ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, Text, View } from "react-native";
 
 import Header from "@/components/header";
 import { Product } from "@/components/product";
 
-import { useCartStore } from "@/stores/cart-store";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import Input from "@/components/input";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
   const cartStore = useCartStore();
   const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0)) // Estude o reduce, interessante
+
+    function handleProductRemove(product: ProductCartProps) {
+        Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+            {
+                text: "Cancelar",
+            }, {
+                text: "Remover",
+                onPress: () => cartStore.remove(product.id)
+            }
+        ])
+    }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
@@ -18,7 +33,7 @@ export default function Cart() {
         {cartStore.products.length > 0 ? (
             <View className="border-b border-slate-700">
           {cartStore.products.map((product) => (
-            <Product data={product} key={product.id} />
+            <Product onPress={() => handleProductRemove(product)} data={product} key={product.id} />
           ))}
         </View>
       ) : (
@@ -33,6 +48,17 @@ export default function Cart() {
       <Input placeholder="Informe o endereço de entrega com Rua, Bairro, CEP, Número e Complemento"/>
       </View>
         </ScrollView>
+        <View className="p-5 gap-5 ">
+            <Button>
+                <Button.Text>
+                    Enviar pedido
+                </Button.Text>
+                <Button.Icon>
+                    <Feather name="arrow-right-circle" size={20}/>
+                </Button.Icon>
+            </Button>
+            <LinkButton href="/"  title="Voltar ao cardápio"/>
+        </View>
     </View>
   );
 }
