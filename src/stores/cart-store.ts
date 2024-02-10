c8e421c251f1947feb8a03dca9c2838dacc2ec38
DFaltGP <skyzzlockzz@gmail.com>
2024-02-10 15:56:47 -0300

    persistindo dados do carrinho no dispositivo do usuário

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,9 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export type ProductCartProps = ProductProps & {
@@ -12,7 +16,8 @@ type StateProps = {
   remove: (productId: string) => void
 };
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
 
   add: (product: ProductProps) =>
@@ -24,4 +29,7 @@ export const useCartStore = create<StateProps>((set) => ({
     set((state) => ({
       products: cartInMemory.remove(state.products, productId)
     }))
+}), {
+  name: "nlw-expert:cart",
+  storage: createJSONStorage(() => AsyncStorage)
 }));
